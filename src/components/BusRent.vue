a30df1410b539a39cda103d267cb4d4ced71d5fe
Dmitrii <dmitrii.petlai@socratechs.com>
2021-10-06 18:31:43 +0300

    small refactor

---
+++ src/components/BusRent.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <section class="container mx-auto px-10 py-10">
+    <div class="flex flex-col md:flex-row bg-gray-600 text-white w-full h-full md:h-96 p-10 h-52 rounded-xl">
+      <div class="flex justify-center flex-col w-full md:w-1/2">
+        <h3 class="text-4xl font-bold mb-4">Аренда микроавтобуса в Орле</h3>
+        <div class="text-base mb-10">Арендуйте восемнадцати местный микроавтобус туристического класса, который оснащен всем необходимым для комфортной поездки</div>
+        <button type="button" class="p-3.5 bg-white text-gray-900 w-auto max-w-min rounded-lg">Арендовать</button>
+      </div>
+      <div class="w-full md:w-1/2 h-80 bg-sprinter bg-no-repeat bg-center bg-contain"></div>
+    </div>
+  </section>
+</template>
