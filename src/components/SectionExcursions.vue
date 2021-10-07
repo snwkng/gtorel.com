73397ef4c7906db7eb079f7fabeb25e0ad31b637
Dmitrii <dmitrii.petlai@socratechs.com>
2021-10-07 18:51:54 +0300

    styles edit

--- src/components/SectionExcursions.vue
+++ src/components/SectionExcursions.vue
@@ -5,12 +5,12 @@ import ExcursionCard from "../components/cards/ExcursionCard.vue"
 <template>
   <section class="container mx-auto px-10 py-10 overflow-hidden">
     <h2 class="text-2xl font-bold mb-8 pr-2 pl-2">Экскурсионные туры из Орла</h2>
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap pr-2 pl-2">
       <excursion-card></excursion-card>
       <excursion-card></excursion-card>
       <excursion-card></excursion-card>
       <excursion-card></excursion-card>
     </div>
-    <div class="text-xl font-semibold">Все экскурсии ></div>
+    <div class="text-xl font-semibold pr-2 pl-2">Все экскурсии ></div>
   </section>
 </template>
